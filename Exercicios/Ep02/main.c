2ca967c49d0670421e09517eabccd18d8c1cbc03
EduardoP98 <purkotee@gmail.com>
2023-09-06 01:30:46 -0300

    Ajustado testes na main

--- Exercicios/Ep02/main.c
+++ Exercicios/Ep02/main.c
@@ -6,8 +6,6 @@
 
 
 
-
-
 int main()
 {
   int mat_ordem;
@@ -25,39 +23,45 @@ int main()
   copiaSistemaLinear(SL,SL_gauss_sem_p );
   copiaSistemaLinear(SL,SL_gauss_sem_mult);
 
-  /*
-  - Passo 1 ElimGauss (Triangularizar Matriz)
-    -Implementar ElimGauss Com pivoteamento Parcial
-    -Implementar ElimGauss Com pivoteamento Total
 
-  - Passo 2 Retrosubs
-  - Passo 3 Resíduo
-  
-  */
+  //1-Forma clássica com pivoteamento
+  printf("Forma clássica com pivoteamento\n");
   double time_i = timestamp();
 
   elimGauss_parcial(SL->A,SL->b,SL->x,SL->n);
-  elimGauss_sem_multiplicadores(SL_gauss_sem_mult->A, SL_gauss_sem_mult->b, SL_gauss_sem_mult->n);
-  elimGauss_sem_pivoteamento(SL_gauss_sem_p->A, SL_gauss_sem_p->b, SL_gauss_sem_p->n);
-
   retrosubs(SL->A,SL->b,SL->x,SL->n);
-  retrosubs(SL_gauss_sem_mult->A, SL_gauss_sem_mult->b,SL_gauss_sem_mult->x, SL_gauss_sem_mult->n);
-  retrosubs(SL_gauss_sem_p->A, SL_gauss_sem_p->b,SL_gauss_sem_p->x, SL_gauss_sem_p->n);
-
   SL->residuo = calculaResiduo(SL->A,SL->b,SL->x,SL->n);
-  SL_gauss_sem_p->residuo = calculaResiduo(SL_gauss_sem_p->A,SL_gauss_sem_p->b,SL_gauss_sem_p->x,SL_gauss_sem_p->n);
-  SL_gauss_sem_mult->residuo = calculaResiduo(SL_gauss_sem_mult->A, SL_gauss_sem_mult->b, SL_gauss_sem_mult->x,SL_gauss_sem_mult->n);
-
   imprime_sistema_linear(SL);
-  imprime_sistema_linear(SL_gauss_sem_mult);
-  imprime_sistema_linear(SL_gauss_sem_p);
-
   libera_sistema_linear(SL);
-  libera_sistema_linear(SL_gauss_sem_p);
-  libera_sistema_linear(SL_gauss_sem_mult);
 
   double time_f = timestamp();
+  printf("Tempo Em MiliSegundos:%lf \n\n",time_f-time_i);
+
+
+  //2-Forma clássica com pivoteamento, sem o cálculo dos multiplicadores 
+  printf("Forma clássica com pivoteamento, sem o cálculo dos multiplicadores\n");
+  time_i = timestamp();
+
+  elimGauss_sem_multiplicadores(SL_gauss_sem_mult->A, SL_gauss_sem_mult->b, SL_gauss_sem_mult->n);
+  retrosubs(SL_gauss_sem_mult->A, SL_gauss_sem_mult->b,SL_gauss_sem_mult->x, SL_gauss_sem_mult->n);
+  SL_gauss_sem_mult->residuo = calculaResiduo(SL_gauss_sem_mult->A, SL_gauss_sem_mult->b, SL_gauss_sem_mult->x,SL_gauss_sem_mult->n);
+  imprime_sistema_linear(SL_gauss_sem_mult);
+  libera_sistema_linear(SL_gauss_sem_mult);  
+  time_f = timestamp();
+  printf("Tempo Em MiliSegundos:%lf \n\n",time_f-time_i);
+
+
+  //3-Forma alternativa onde não usa pivoteamento
+  printf("Forma alternativa onde não usa pivoteamento\n");
+  time_i = timestamp();
+  elimGauss_sem_pivoteamento(SL_gauss_sem_p->A, SL_gauss_sem_p->b, SL_gauss_sem_p->n);
+  retrosubs(SL_gauss_sem_p->A, SL_gauss_sem_p->b,SL_gauss_sem_p->x, SL_gauss_sem_p->n);  
+  SL_gauss_sem_p->residuo = calculaResiduo(SL_gauss_sem_p->A,SL_gauss_sem_p->b,SL_gauss_sem_p->x,SL_gauss_sem_p->n);
+  imprime_sistema_linear(SL_gauss_sem_p);
+  libera_sistema_linear(SL_gauss_sem_p);
   
+
+  time_f = timestamp();
   printf("Tempo Em MiliSegundos:%lf \n\n",time_f-time_i);
 
   return 0;
