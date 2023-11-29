bd223b937eccacf86af1c41434cdb0d2ca7b1e0c
Mariana Moreira <mariana_moreira0@hotmail.com>
2023-11-29 01:17:43 -0300

    Resultados parte 2

--- Trabalhos/T2/bash.sh
+++ Trabalhos/T2/bash.sh
@@ -1,80 +1,80 @@
-#!/bin/bash
+!/bin/bash
 
 if [ ! -d "informacoes" ]; then
-    # Crie a pasta se ela não existir
+    Crie a pasta se ela não existir
     mkdir "informacoes"
 fi
 
-# Topologia
+Topologia
 echo "Criando arquivo contendo informações sobre a topologia..."
 likwid-topology -g -c > informacoes/likwid_topology.txt
 echo
 
-# Frequencia do processador
+Frequencia do processador
 echo  "Fixando frequência do processador..."
 chmod a+rw /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
 echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
 
-# Modulos
+Modulos
 echo
 echo "Carregando módulo msr"
 echo modprobe msr
 
-# Nucleo
+Nucleo
 CORE_ID=1
 echo "Utilizando o core ${CORE_ID}..."
 
-# Teste unico
-# likwid-perfctr -C ${CORE_ID} -g L3 -m ./matmult 64 > debug.txt
+Teste unico
+likwid-perfctr -C ${CORE_ID} -g L3 -m ./matmult 64 > debug.txt
 
-# Iniciando testes
-# echo
-# echo "Iniciando testes de desempenho..." 
-# echo "Utilizando a Versão 1"
-# echo
+Iniciando testes
+echo
+echo "Iniciando testes de desempenho..." 
+echo "Utilizando a Versão 1"
+echo
 
-# cd v1
-# # Verifique se a pasta já existe
-# if [ ! -d "resultados" ]; then
-#     # Crie a pasta se ela não existir
-#     mkdir "resultados"
-#     mkdir "resultados/tempo"
-# fi
+cd v1
+Verifique se a pasta já existe
+if [ ! -d "resultados" ]; then
+    Crie a pasta se ela não existir
+    mkdir "resultados"
+    mkdir "resultados/tempo"
+fi
 
-# # Verifica se o programa está disponível
-# if [ ! -f ajustePol ]; then
-#   echo  
-#   echo "O programa não foi encontrado. Compilando..."
-#   echo
-#   make
-#   if [ $? -ne 0 ]; then
-#     echo
-#     echo "Falha na compilação do programa."
-#     exit 1
-#   fi
-# fi
+Verifica se o programa está disponível
+if [ ! -f ajustePol ]; then
+  echo  
+  echo "O programa não foi encontrado. Compilando..."
+  echo
+  make
+  if [ $? -ne 0 ]; then
+    echo
+    echo "Falha na compilação do programa."
+    exit 1
+  fi
+fi
 
-# echo
-# echo "Programa compilado. Iniciando preparação do ambiente..."
-# echo
+echo
+echo "Programa compilado. Iniciando preparação do ambiente..."
+echo
 
-# for n in 64 128 200 256 512 600 800 1024 2000 3000 4096 6000 7000 10000 50000 100000 1000000 10000000 100000000 
-# do
+for n in 64 128 200 256 512 600 800 1024 2000 3000 4096 6000 7000 10000 50000 100000 1000000 10000000 100000000 
+do
 
-#   echo "N = $n"
-  # ./gera_entrada $n|  ./ajustePol >  resultados/tempo/$n.txt
-  # ./gera_entrada $n  | likwid-perfctr -C ${CORE_ID} -g L3 -m ./ajustePol  > resultados/L3_$n.txt
-  # ./gera_entrada $n  | likwid-perfctr -C ${CORE_ID} -g L2CACHE -m ./ajustePol > resultados/L2CACHE_$n.txt
-  # ./gera_entrada $n  | likwid-perfctr -C ${CORE_ID} -g FLOPS_DP -m ./ajustePol > resultados/FLOPS_DP_$n.txt
+  echo "N = $n"
+  ./gera_entrada $n|  ./ajustePol >  resultados/tempo/$n.txt
+  ./gera_entrada $n  | likwid-perfctr -C ${CORE_ID} -g L3 -m ./ajustePol  > resultados/L3_$n.txt
+  ./gera_entrada $n  | likwid-perfctr -C ${CORE_ID} -g L2CACHE -m ./ajustePol > resultados/L2CACHE_$n.txt
+  ./gera_entrada $n  | likwid-perfctr -C ${CORE_ID} -g FLOPS_DP -m ./ajustePol > resultados/FLOPS_DP_$n.txt
     
-# done
-# echo "Pronto! Arquivos criados no diretório resultados..."
-# echo
-# # # Limpando
-# echo
-# echo "Finalizando execução e removendo arquivos temporários"
-# make purge
-# cd ..
+done
+echo "Pronto! Arquivos criados no diretório resultados..."
+echo
+# Limpando
+echo
+echo "Finalizando execução e removendo arquivos temporários"
+make purge
+cd ..
 
 
 # echo "Iniciando testes de desempenho... Utilizando a Versão 2"
@@ -116,21 +116,21 @@ done
 echo "Pronto! Arquivos criados no diretório resultados..."
 echo
 
-# sleep 1
+sleep 1
 
-# # # Limpando
-# echo
-# echo "Finalizando execução e removendo arquivos temporários"
-# make purge
+# # Limpando
+echo
+echo "Finalizando execução e removendo arquivos temporários"
+make purge
 
-# cd ..
+cd ..
 
-# # Frequencia original
-# echo
-# echo "Retornando computador a frequencia original..."
-# echo "powersave" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
+# Frequencia original
+echo
+echo "Retornando computador a frequencia original..."
+echo "powersave" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
 
-# # Mensagem final
-# echo
-# echo "Pronto!"
-# echo
+# Mensagem final
+echo
+echo "Pronto!"
+echo
